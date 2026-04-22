16ae15b8743581fa1ead8fa2071fb040c16ef203
Zhang Jingqiang <zh_jq@outlook.com>
2026-04-22 23:37:47 +0800

    add missing copyright notes

--- lib/vey-ftp-client/src/control/response.rs
+++ lib/vey-ftp-client/src/control/response.rs
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  * Copyright 2023-2025 ByteDance and/or its affiliates.
- * Copyright 2026 VEY-OSS developers.
+ * Copyright 2026 VEY-OSS Developers.
  */
 
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
--- lib/vey-h2/src/client/mod.rs
+++ lib/vey-h2/src/client/mod.rs
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
- * Copyright 2026 VEY-OSS developers.
+ * Copyright 2026 VEY-OSS Developers.
  */
 
 mod response;
