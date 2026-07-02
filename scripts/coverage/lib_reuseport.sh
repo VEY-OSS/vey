371ad10e0c304df5c14ae51b4916e2ad17e23099
Zhang Jingqiang <zh_jq@outlook.com>
2026-07-02 23:04:25 +0800

    CI: add a standalone reuseport coverage test

---
+++ scripts/coverage/lib_reuseport.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+
+SCRIPTS_DIR=$(dirname "$0")
+PROJECT_DIR=$(realpath "${SCRIPTS_DIR}/../..")
+
+
+TEST_NAME="reuseport-test"
+. "${SCRIPTS_DIR}/enter.sh"
+
+# build
+cargo build -p vey-reuseport
+
+all_binaries=$(find target/debug/ -maxdepth 1 -type f -perm /111 | awk '{print "-object "$0}')
+
+# run the tests
+sudo "${PROJECT_DIR}"/target/debug/udp_reuseport_test
+
+all_objects=$(find target/debug/deps/ -type f -perm /111 -not -name "*.so" | awk '{print "-object "$0}')
+
+
+# get all profraw files generated in each test
+profraw_files=$(find . -type f -regex ".*/${TEST_NAME}.*\.profraw")
+
+# get indexed profile data file
+cargo profdata -- merge -o "${PROF_DATA_FILE}" ${profraw_files}
+
+# report to console
+
+IGNORE_FLAGS="--ignore-filename-regex=.cargo \
+    --ignore-filename-regex=rustc \
+    --ignore-filename-regex=target/debug/build"
+
+cargo cov -- report --use-color --instr-profile="${PROF_DATA_FILE}" ${IGNORE_FLAGS} ${all_binaries} ${all_objects}
+cargo cov -- export --format=lcov --instr-profile="${PROF_DATA_FILE}" ${IGNORE_FLAGS} ${all_binaries} ${all_objects} > output.lcov
