b1e88380089644d352bf1d904c902c30d93d04d7
Zhang Jingqiang <zh_jq@outlook.com>
2026-08-15 23:31:25 +0800

    cargo fmt

--- lib/vey-journal/src/io.rs
+++ lib/vey-journal/src/io.rs
@@ -11,13 +11,13 @@ use std::os::fd::AsFd;
 use std::os::unix::net::UnixDatagram;
 
 use anyhow::{Context, anyhow};
+use once_cell::sync::OnceCell;
 use rustix::cmsg_space;
 use rustix::fs::{MemfdFlags, SealFlags, fcntl_add_seals, memfd_create};
 use rustix::io::Errno;
 use rustix::net::{
     SendAncillaryBuffer, SendAncillaryMessage, SendFlags, SocketAddrUnix, sendmsg_addr,
 };
-use once_cell::sync::OnceCell;
 
 /// Default path of the systemd-journald `AF_UNIX` datagram socket.
 const SD_JOURNAL_SOCK_PATH: &str = "/run/systemd/journal/socket";
