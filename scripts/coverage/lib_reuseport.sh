c3ff8e2c46c904486249e112a97213b5096ee66b
Zhang Jingqiang <zh_jq@outlook.com>
2026-09-12 23:59:23 +0800

    fix reuseport test bin name

--- scripts/coverage/lib_reuseport.sh
+++ scripts/coverage/lib_reuseport.sh
@@ -15,9 +15,9 @@ cargo build -p vey-reuseport
 all_binaries=$(find target/debug/ -maxdepth 1 -type f -perm /111 | awk '{print "-object "$0}')
 
 # run the tests
-sudo --preserve-env=LLVM_PROFILE_FILE "${PROJECT_DIR}"/target/debug/udp_reuseport_test
-sudo --preserve-env=LLVM_PROFILE_FILE "${PROJECT_DIR}"/target/debug/tcp_reuseport_test
-sudo --preserve-env=LLVM_PROFILE_FILE "${PROJECT_DIR}"/target/debug/quic_reuseport_test
+sudo --preserve-env=LLVM_PROFILE_FILE "${PROJECT_DIR}"/target/debug/udp-reuseport-test
+sudo --preserve-env=LLVM_PROFILE_FILE "${PROJECT_DIR}"/target/debug/tcp-reuseport-test
+sudo --preserve-env=LLVM_PROFILE_FILE "${PROJECT_DIR}"/target/debug/quic-reuseport-test
 
 all_objects=$(find target/debug/deps/ -type f -perm /111 -not -name "*.so" | awk '{print "-object "$0}')
 
